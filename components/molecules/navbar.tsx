5583424dcd5789b6783dd407044eda12dcfaf28a
LianggaRistiana <liangga4@gmail.com>
2025-07-25 10:43:41 +0800

    feat: add navbar

---
+++ components/molecules/navbar.tsx
@@ -0,0 +1,14 @@
+// import { ModeToggle } from "./mode-toggle";
+
+
+export default function Navbar() {
+    return (
+        <nav className="fixed top-0 w-full gap-4 z-50 flex items-center justify-between py-4 px-4 lg:px-12 xl:px-24 backdrop-blur-lg  ">
+            <p  className="text-xl font-bold text-primary">
+                Pesona Kaba-Kaba
+            </p>
+            {/* <ModeToggle></ModeToggle> */}
+        </nav>
+    );
+
+}
